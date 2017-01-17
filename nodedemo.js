9e53ef3fbbbb9de3777874d67c1cba2baa5efd22
sidsingh760 <sidsingh760@gmail.com>
2017-01-17 14:51:08 +0530

    Add files via upload

---
+++ nodedemo.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var server = app.listen(8081, function () {
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
+app.get('/listUsers', function (req, res) {
+   fs.readFile("user.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+var user = {
+   "user4" : {
+       "name" : "mohit",
+       "password" : "password4",
+       "profession" : "teacher",
+       "id": 4
+   }
+}
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+       user = JSON.parse( data );
+       var use = user["user" + req.params.id]
+       console.log( use );
+       res.end( JSON.stringify(use));
+   });
+})
+var id = 2;
+
+app.delete('/:id', function (req, res) {
+
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data["user" + req.params.id];
+
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
